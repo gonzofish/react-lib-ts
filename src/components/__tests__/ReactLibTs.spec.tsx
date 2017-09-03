44dd12b47c98c9e2219ae2f1bd15d27ac7aa1b7b
Matt Fehskens <matt.fehskens@gmail.com>
2017-09-03 09:50:28 -0400

    Fixed tests for ReactLibTs component

--- src/components/__tests__/ReactLibTs.spec.tsx
+++ src/components/__tests__/ReactLibTs.spec.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import { HelloYou } from '../HelloYou';
 import { ReactLibTs } from '../ReactLibTs';
 
 describe('Component: ReactLibTs', () => {
     test('should render without error', () => {
-        expect(shallow(<ReactLibTs />).contains(<div>ReactLibTs</div>)).toBe(true);
+        expect(shallow(<ReactLibTs />).contains(<HelloYou />)).toBe(true);
     });
 });
